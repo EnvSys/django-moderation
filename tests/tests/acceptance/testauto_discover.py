f567a494031ed138af8c30586627ab2440672db6
Maximilian Friedersdorff <maximilian.friedersdorff@envsys.co.uk>
2024-08-13 12:10:06 +0100

    Fix auto discovery test

--- tests/tests/acceptance/testauto_discover.py
+++ tests/tests/acceptance/testauto_discover.py
@@ -2,7 +2,6 @@ import sys
 
 from django.test.testcases import TestCase
 
-from moderation.helpers import auto_discover
 from tests.utils import setup_moderation, teardown_moderation
 
 
@@ -21,5 +20,4 @@ class AutoDiscoverAcceptanceTestCase(TestCase):
         teardown_moderation()
 
     def test_all_app_containing_moderator_module_should_be_registered(self):
-        auto_discover()
         self.assertTrue("tests.moderator" in sys.modules.keys())
