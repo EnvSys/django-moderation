dbd3ec1f6dcb9b217e4977b9b02b1d4aa46a21a9
chubi <chubiyojo.adejoh@envsys.co.uk>
2026-03-11 15:04:49 +0000

    replace url with newer path method

--- tests/urls/auto_discover.py
+++ tests/urls/auto_discover.py
@@ -1,12 +1,10 @@
 from django.conf import settings
-from django.conf.urls import include, handler500, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import path
 
 admin.autodiscover()
 
-handler500
-
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-].extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+    path("admin/", admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
